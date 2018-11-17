2c606456635ebde34ce6c3f8b9fb7fd563211fb2
Tom Konijn <tom@ambrero.nl>
2018-11-17 19:26:10 +0100

    Blazor en Shared added

---
+++ Api/Controllers/TeamController.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Web.ViewModels;
+
+namespace Web.Controllers
+{
+    public class TeamController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            return View(new List<Team>
+            {
+                new Team
+                {
+                    Name = "Sdv Installatie",
+                    ColorShirt = "Wit",
+                    ColorShort = "Zwart",
+                    TeamPictureUrl = "/images/teams/2016/SDV%20installatietechniek.JPG"
+                },
+                new Team
+                {
+                    Name = "Ander team",
+                    ColorShirt = "Groen",
+                    ColorShort = "Rood",
+                    TeamPictureUrl = "/images/teams/2017/SDV%20installatietechniek.JPG"
+                }
+            });
+        }
+    }
+}
