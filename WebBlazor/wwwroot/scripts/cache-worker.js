856e3d3a872c3c3bba11e79d589077111ed98f5c
Tom Konijn <tom@ambrero.nl>
2018-11-18 21:54:56 +0100

    Web removed.

---
+++ WebBlazor/wwwroot/scripts/cache-worker.js
@@ -0,0 +1,36 @@
+﻿var CacheWorker = (function () {
+
+    let cacheServiceWorkerRegistration;
+
+    function writeToConsole(text) {
+        console.log(text);
+    };
+
+    function registerCahceServiceWorker() {
+        navigator.serviceWorker.register('/cache-service-worker.js', { scope: '/' })
+            .then(function (serviceWorkerRegistration) {
+                cacheServiceWorkerRegistration = serviceWorkerRegistration;
+
+                // initializeUIState();
+
+                writeToConsole('Cache Service Worker has been registered successfully');
+            }).catch(function (error) {
+            writeToConsole('Cache Service Worker registration has failed: ' + error);
+        });
+    };
+    
+    return {
+        initialize: function () {
+            // initializeConsole();
+
+            if (!'serviceWork' in navigator) {
+                writeToConsole('Service Workers are not supported');
+                return;
+            }
+
+            registerCahceServiceWorker();
+        }
+    };
+})();
+
+CacheWorker.initialize();
