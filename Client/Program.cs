cc753ea56031e2c5b7889d5e41587365cc2f2f42
Tom Konijn <tom@ambrero.nl>
2018-11-18 17:39:17 +0100

    Web removed.

---
+++ Client/Program.cs
@@ -0,0 +1,20 @@
+﻿using Client.Services;
+using Microsoft.AspNetCore.Blazor.Browser.Rendering;
+using Microsoft.AspNetCore.Blazor.Browser.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Client
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceProvider = new BrowserServiceProvider(services =>
+            {
+                services.AddSingleton<PokeState>();
+            });
+
+            new BrowserRenderer(serviceProvider).AddComponent<Main>("app");
+        }
+    }
+}
